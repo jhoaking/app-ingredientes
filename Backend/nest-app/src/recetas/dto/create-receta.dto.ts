0030b26184a4dfcdd49ae4a9de7fbde6abd41139
jhoaking <peredoroj@gmail.com>
2025-07-24 10:27:55 -0400

    dtos ya definidos

--- Backend/nest-app/src/recetas/dto/create-receta.dto.ts
+++ Backend/nest-app/src/recetas/dto/create-receta.dto.ts
@@ -1,1 +1,43 @@
-export class CreateRecetaDto {}
+import {
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateRecetaDto {
+  @IsString()
+  @MinLength(1)
+  nombre: string;
+
+  @IsString()
+  @IsOptional()
+  descripcion?: string;
+
+  @IsString()
+  @MinLength(2)
+  categorias: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  rate?: number;
+
+  @IsString()
+  @MinLength(1)
+  preparacion: string;
+
+  @IsString()
+  pais?: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  ingredientes: string[];
+
+  @IsString({ each: true })
+  @IsOptional()
+  @IsArray()
+  images?: string[];
+}
